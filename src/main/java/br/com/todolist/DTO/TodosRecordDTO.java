1b5e676f1d4de01b48ff81ceca31020e14ae0fd3
LacerdaT <thiagoduraesdelacerda@gmail.com>
2024-12-29 21:51:55 -0300

    Renamed the parameters

--- src/main/java/br/com/todolist/DTO/TodosRecordDTO.java
+++ src/main/java/br/com/todolist/DTO/TodosRecordDTO.java
@@ -1,9 +1,8 @@
 package br.com.todolist.DTO;
 
-import java.util.UUID;
-
-public record TodosRecordDTO(String nome,
-                             String descricao,
-                             int prioridade,
-                             boolean realizado) {
+public record TodosRecordDTO(String name,
+                             String description,
+                             int priority,
+                             boolean completed) {
 }
+
