6c6679fd2a1ae22fc71cc38127fc95d2ae8c0679
LacerdaT <thiagoduraesdelacerda@gmail.com>
2024-12-30 02:01:10 -0300

    Removed unnecessary import

--- src/main/java/br/com/todolist/Repository/TodosRepository.java
+++ src/main/java/br/com/todolist/Repository/TodosRepository.java
@@ -1,10 +1,8 @@
 package br.com.todolist.Repository;
 
 import br.com.todolist.Model.TodosModel;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface  TodosRepository extends JpaRepository<TodosModel, UUID> {
