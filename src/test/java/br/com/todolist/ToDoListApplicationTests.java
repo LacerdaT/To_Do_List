b0545e276a8a1fdb4d39c17aba293ad761c904da
LacerdaT <thiagoduraesdelacerda@gmail.com>
2024-12-30 00:16:56 -0300

    implement unit tests

--- src/test/java/br/com/todolist/ToDoListApplicationTests.java
+++ src/test/java/br/com/todolist/ToDoListApplicationTests.java
@@ -1,13 +1,45 @@
 package br.com.todolist;
 
+import br.com.todolist.Model.PriorityRole;
+import br.com.todolist.Model.TodosModel;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ToDoListApplicationTests {
 
+	@Autowired
+	private WebTestClient webTestClient;
+
 	@Test
-	void contextLoads() {
+	void todosTestSuccess() {
+		var todos = new TodosModel("Teste 1", "Teste descrição 1", PriorityRole.LOW, false);
+
+		webTestClient
+				.post()
+				.uri("/todoslist/todos")
+				.bodyValue(todos)
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody()
+				.jsonPath("$.name").isEqualTo(todos.getName())
+				.jsonPath("$.description").isEqualTo(todos.getDescription())
+				.jsonPath("$.priority").isEqualTo(todos.getPriority())
+				.jsonPath("$.completed").isEqualTo(todos.isCompleted());
+	}
+
+	@Test
+	void todosTestFailure() {
+		webTestClient
+				.post()
+				.uri("/todoslist/todos")
+				.bodyValue(
+						new TodosModel("", "", PriorityRole.HIGH, false)
+				).exchange()
+				.expectStatus().isBadRequest();
+
 	}
 
 }
